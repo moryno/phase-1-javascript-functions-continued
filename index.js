58898320b4de6a5c191a9958701b5d4b3f507ea3
Maurice <mauricenganga41@gmail.com>
2022-06-06 20:54:08 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 // code your solution here
 const saturdayFun = (event ="roller-skate" ) =>{
-   return `This Saturday, I want to ${event}`
+   return `This Saturday, I want to ${event}!`
 };
+
+const mondayWork = (toDO = "go to the office") => {
+    return `This Monday, I will ${toDO}.`
+}
+
+const wrapAdjective = (flair = "*") => {
+    return (adj = "special") =>{
+       return `You are ${flair}${adj}${flair}!`
+    }
+}
